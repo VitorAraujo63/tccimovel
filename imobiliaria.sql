f20662e104637491cb18284bf462776a46539554
Vitor Araujo <VitorAraujo63>
2023-12-04 22:23:01 -0300

    Ajustes Finais

--- imobiliaria.sql
+++ imobiliaria.sql
@@ -1,25 +1,48 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.11.2-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.3.0.6295
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `telefone` varchar(14) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
--- Copiando estrutura do banco de dados para imobiliaria.tcc
-DROP DATABASE IF EXISTS `imobiliaria.tcc`;
-CREATE DATABASE IF NOT EXISTS `imobiliaria.tcc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `imobiliaria.tcc`;
+INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`) VALUES
+	(1, 'gabriel', 'vitorvirgem@gamil.com', NULL, '123456'),
+	(2, 'amando', 'dwadjwv@gmail.com', '5514990336607', '43234234'),
+	(3, 'danilo', 'sambandocomvc@gmail.com', '55149912345677', '948293'),
+	(4, 'Cristo', 'vidajesus@gmail.com', '551499543454', '463456'),
+	(5, 'lllllllllll', 'sentido@gmail.com', '551499543454', '867567'),
+	(6, 'rrrrrrrrr', 'NAOALBERTO@gmail.com', '551499543974', '45484556'),
+	(7, 'Proxa', 'vidajesus@gmail.com', '84939374574', '85634097'),
+	(8, 'Vitor', 'vitinhoaraujo201380@gmail.com', '14999031741', '123456');
 
--- Copiando estrutura para tabela imobiliaria.tcc.imagens_imoveis
-DROP TABLE IF EXISTS `imagens_imoveis`;
+CREATE TABLE IF NOT EXISTS `imovel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quarto` int(11) DEFAULT NULL,
+  `banheiro` int(11) DEFAULT NULL,
+  `preco` decimal(10,0) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `rua` varchar(50) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `cep` varchar(40) DEFAULT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_imovel_usuario` (`id_usuario`),
+  CONSTRAINT `fk_imovel_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+INSERT INTO `imovel` (`id`, `quarto`, `banheiro`, `preco`, `estado`, `cidade`, `rua`, `bairro`, `numero`, `cep`, `descricao`, `id_usuario`) VALUES
+	(39, 2, 1, 275000, 'Santa Catarina', 'Jaguaruna', 'Riachinho', NULL, NULL, '88715000', 'CASA DE ALVENARIA GEMINADA NO RIACHINHO (LOT. MOINHO DE VENTO) - JAGUARUNA/SC  Possuindo:  * 2 Quartos * Sala e cozinha conjugadas * Banheiro * Área de serviço * Laje inclinada * Teto em gesso * Garagem * Cercada * Murada * Escriturada  * Área total construída: 68,00 m² * Área total do terreno: 173,29 m² (6,66x26)  * Localizada a 2,5 km do Centro, e a 8 km do mar.', 8),
+	(41, 3, 2, 630000, 'Santa Catarina', 'Jaguaruna', 'Arroio Corrente', NULL, NULL, '88715000', 'CASA DE ALVENARIA COM LAGE INCLINADA NO BALNEÁRIO ARROIO CORRENTE (LOT. CASCATA VERMELHA) - JAGUARUNA/SC  Imóvel possui:  * 3 Quartos * 2 Salas * Cozinha (Sob medida) * Banheiro (Sob medida) * Garagem p/ 3 veiculos  * Churrasqueira * Piso porcelanato * Aberturas de aluminio * Frente voltada para o mar', 8),
+	(42, 4, 2, 426000, 'Sao Paulo', 'Marília', 'Hermes da fonseca', 'Palmital', 1448, '17511403', 'CASA MISTA NO PALMITAL - MARÍLIA/SP  POSSUINDO:  * 4 Quartos * Cozinha * Sala * 2 Banheiros * Garagem para 3 veículos * Despensa * Área de serviço * Churrasqueira * Murada * Escriturada * Mobiliada  * Área total construída: 150,00 m² * Área total do terreno: 304,50 m² (14,50x21,00)  * Ótima localização, a 200 metros do chuveirão e a 500 metros do mar.  Mobília sim', 8);
+
+
 CREATE TABLE IF NOT EXISTS `imagens_imoveis` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `arquivo` text DEFAULT NULL,
@@ -29,9 +52,6 @@ CREATE TABLE IF NOT EXISTS `imagens_imoveis` (
   CONSTRAINT `fk_imagens_imovel` FOREIGN KEY (`id_imovel`) REFERENCES `imovel` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela imobiliaria.tcc.imagens_imoveis: ~29 rows (aproximadamente)
-DELETE FROM `imagens_imoveis`;
-/*!40000 ALTER TABLE `imagens_imoveis` DISABLE KEYS */;
 INSERT INTO `imagens_imoveis` (`id`, `arquivo`, `id_imovel`) VALUES
 	(57, '/storage/fotos/FFE5xcHOfPgowLuGBgeuqphoEvb7YwC1lmpmzjLj.jpg', 39),
 	(58, '/storage/fotos/HbddBJjzOePKYRZVyZy545UbsoK8t7IyhE4BSc2w.jpg', 39),
@@ -62,63 +82,3 @@ INSERT INTO `imagens_imoveis` (`id`, `arquivo`, `id_imovel`) VALUES
 	(92, '/storage/fotos/CST9qwrpNJv2K6hicMPPSKACbOoQ5O2qi4bc6YVO.jpg', 42),
 	(93, '/storage/fotos/PLCsu24SFi1v0ZxqFGvcAa6eTzNUO1VlQTuDJ0L8.jpg', 42),
 	(94, '/storage/fotos/LIPHyW146h2NLqJHwWk9oLOpXhyJxbdcE3rA1jGi.jpg', 42);
-/*!40000 ALTER TABLE `imagens_imoveis` ENABLE KEYS */;
-
--- Copiando estrutura para tabela imobiliaria.tcc.imovel
-DROP TABLE IF EXISTS `imovel`;
-CREATE TABLE IF NOT EXISTS `imovel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `quarto` int(11) DEFAULT NULL,
-  `banheiro` int(11) DEFAULT NULL,
-  `preco` decimal(10,0) NOT NULL,
-  `estado` varchar(50) NOT NULL,
-  `cidade` varchar(50) DEFAULT NULL,
-  `rua` varchar(50) DEFAULT NULL,
-  `bairro` varchar(50) DEFAULT NULL,
-  `numero` int(11) DEFAULT NULL,
-  `cep` varchar(40) DEFAULT NULL,
-  `descricao` varchar(500) DEFAULT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_imovel_usuario` (`id_usuario`),
-  CONSTRAINT `fk_imovel_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Copiando dados para a tabela imobiliaria.tcc.imovel: ~4 rows (aproximadamente)
-DELETE FROM `imovel`;
-/*!40000 ALTER TABLE `imovel` DISABLE KEYS */;
-INSERT INTO `imovel` (`id`, `quarto`, `banheiro`, `preco`, `estado`, `cidade`, `rua`, `bairro`, `numero`, `cep`, `descricao`, `id_usuario`) VALUES
-	(39, 2, 1, 275000, 'Santa Catarina', 'Jaguaruna', 'Riachinho', NULL, NULL, '88715000', 'CASA DE ALVENARIA GEMINADA NO RIACHINHO (LOT. MOINHO DE VENTO) - JAGUARUNA/SC  Possuindo:  * 2 Quartos * Sala e cozinha conjugadas * Banheiro * Área de serviço * Laje inclinada * Teto em gesso * Garagem * Cercada * Murada * Escriturada  * Área total construída: 68,00 m² * Área total do terreno: 173,29 m² (6,66x26)  * Localizada a 2,5 km do Centro, e a 8 km do mar.', 8),
-	(41, 3, 2, 630000, 'Santa Catarina', 'Jaguaruna', 'Arroio Corrente', NULL, NULL, '88715000', 'CASA DE ALVENARIA COM LAGE INCLINADA NO BALNEÁRIO ARROIO CORRENTE (LOT. CASCATA VERMELHA) - JAGUARUNA/SC  Imóvel possui:  * 3 Quartos * 2 Salas * Cozinha (Sob medida) * Banheiro (Sob medida) * Garagem p/ 3 veiculos  * Churrasqueira * Piso porcelanato * Aberturas de aluminio * Frente voltada para o mar', 8),
-	(42, 4, 2, 426000, 'Sao Paulo', 'Marília', 'Hermes da fonseca', 'Palmital', 1448, '17511403', 'CASA MISTA NO PALMITAL - MARÍLIA/SP  POSSUINDO:  * 4 Quartos * Cozinha * Sala * 2 Banheiros * Garagem para 3 veículos * Despensa * Área de serviço * Churrasqueira * Murada * Escriturada * Mobiliada  * Área total construída: 150,00 m² * Área total do terreno: 304,50 m² (14,50x21,00)  * Ótima localização, a 200 metros do chuveirão e a 500 metros do mar.  Mobília sim', 8);
-/*!40000 ALTER TABLE `imovel` ENABLE KEYS */;
-
--- Copiando estrutura para tabela imobiliaria.tcc.usuario
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(60) DEFAULT NULL,
-  `email` varchar(80) DEFAULT NULL,
-  `telefone` varchar(14) DEFAULT NULL,
-  `senha` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Copiando dados para a tabela imobiliaria.tcc.usuario: ~7 rows (aproximadamente)
-DELETE FROM `usuario`;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`id`, `nome`, `email`, `telefone`, `senha`) VALUES
-	(1, 'gabriel', 'vitorvirgem@gamil.com', NULL, '123456'),
-	(2, 'amando', 'dwadjwv@gmail.com', '5514990336607', '43234234'),
-	(3, 'danilo', 'sambandocomvc@gmail.com', '55149912345677', '948293'),
-	(4, 'Cristo', 'vidajesus@gmail.com', '551499543454', '463456'),
-	(5, 'lllllllllll', 'sentido@gmail.com', '551499543454', '867567'),
-	(6, 'rrrrrrrrr', 'NAOALBERTO@gmail.com', '551499543974', '45484556'),
-	(7, 'Proxa', 'vidajesus@gmail.com', '84939374574', '85634097'),
-	(8, 'Vitor', 'vitinhoaraujo201380@gmail.com', '14999031741', '123456');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
